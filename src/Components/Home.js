292bc08daa9c6dd7960a7a0b29be0cbe182a647a
Winston Chen <winstonchen.wm@gmail.com>
2021-04-22 14:48:56 -0400

    Home Update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -126,24 +126,32 @@ const Home = ({slides}) => {
     const length = slides.length
     const timeout = useRef(null)
 
-    useEffect(() => {
-        const nextSlide = () => {
-            setCurrent(current => (current === length - 1 ? 0 : current + 1))
-        }
-        timeout.current = setTimeout(nextSlide, 2000)
+    // useEffect(() => {
+    //     const nextSlide = () => {
+    //         setCurrent(current => (current === length - 1 ? 0 : current + 1))
+    //     }
+    //     timeout.current = setTimeout(nextSlide, 1000)
 
-        return function () {
-            if(timeout.current) {
-                clearTimeout(timeout.current);
-            }
-        }
-    }, [current, length])
+    //     return function () {
+    //         if(timeout.current) {
+    //             clearTimeout(timeout.current);
+    //         }
+    //     }
+    // }, [current, length])
 
     const nextSlide = () => {
+        // if(timeout.current) {
+        //     clearTimeout(timeout.current);
+        // }
+
         setCurrent(current === length - 1 ? 0 : current + 1)
     }
 
     const prevSlide = () => {
+        // if(timeout.current) {
+        //     clearTimeout(timeout.current);
+        // }
+
         setCurrent(current ===  0 ? length - 1 : current - 1)
     }
 
